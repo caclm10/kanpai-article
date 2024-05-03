542cacf8cd6f8b9131897df87aa3c4aa76e8a2ea
caclm10 <lewinxander@gmail.com>
2024-05-03 19:39:50 +0700

    create action tabs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import ActionTab from "@/components/ActionTab";
+import ActionTabs from "@/components/ActionTabs";
 import Header from "@/components/Header";
 
 export default function Article() {
     return (
         <>
             <Header title="Article" />
+
+            <ActionTabs>
+                <ActionTab title="Article" label="List Article" isActive />
+                <ActionTab title="Add / Edit" label="Detail Article" />
+            </ActionTabs>
         </>
     );
 }
