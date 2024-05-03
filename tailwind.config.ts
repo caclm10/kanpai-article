05f1b95f301402b3741fd9ab096bc9d2c097a357
caclm10 <lewinxander@gmail.com>
2024-05-03 16:56:37 +0700

    add background color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,9 @@ const config: Config = {
         orange: {
           DEFAULT: "#CF8812",
           foreground: "#f8fafc"
+        },
+        background: {
+          DEFAULT: "#f8fafc"
         }
       },
     },
